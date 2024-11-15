bee130c3edf22a45c431777e9b9c7b555e4e3f7e
ToasterPanic <91223726+ToasterPanic@users.noreply.github.com>
2024-11-15 01:00:20 +0000

    Push 2.0-beta1

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,3 +1,5 @@
+var config = {}
+
 $("#url-name").on("input", (e)=>{
     var test = RegExp("^[a-zA-Z0-9]*$", "gm").exec($("#url-name").val())
     if (!test) {
@@ -16,6 +18,18 @@ $("#shortened-url").on("input", (e)=>{
     }
 })
 
+$("#password").on("input", (e)=>{
+    $("#password-incorrect").css("display", "none")
+})
+
+$("#create-new-link").on("click", (e)=>{
+    $("#url-created").css("display", "none")
+    $("#create").css("display", "block")
+
+    $("#shortened-url").val("")
+    $("#url-name").val("")
+})
+
 $("#submit").on("click", (e)=>{
     $.ajax({
         type: "POST",
@@ -26,7 +40,40 @@ $("#submit").on("click", (e)=>{
             password: $("#password").val(),
         },
         success: function(data) {
-            $("body").html(JSON.stringify(data));
+            try {
+                if (!data.success) {
+                    if (data.error == "password-incorrect") {
+                        $("#password-incorrect").css("display", "block")
+                    }
+                    return
+                }
+                var link = "https://" + document.location.host + "/" + data.name
+
+                $("#url-created").css("display", "block")
+                $("#create").css("display", "none")
+
+                $("#new-url-link").attr("href", link)
+                $("#new-url-link").text(link)
+
+                //$("body").html(JSON.stringify(data));
+            } catch(e) {
+                alert(e)
+            }
         }
     });
 })
+
+$.ajax({
+    type: "GET",
+    url: "/config.json",
+    success: function(data) {
+        try {
+            config = data
+            if (data.passwordProtected) {
+                $("#password-box").css("display", "block")
+            }
+        } catch(e) {
+            alert(e)
+        }
+    }
+});
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const fs = require('node:fs');
 
-const config = require("./config.js")                 // Config data here
+var config = {}
+
+try {
+  require("./config.js")                                          // Check if config file exists
+} catch(e) {                                                      // If it doesn't, make one!
+  var defaultConfigValue = fs.readFileSync('./config.default.js') // Yes, there's better ways,
+  fs.writeFileSync("./config.js", defaultConfigValue)             // but I don't care!
+}
+
+config = require("./config.js")
 
 const express = require("express") 
 var app = express()                                   // Setup ExpressJS server
@@ -15,77 +24,96 @@ try {
   console.error(err);
 }
 
-function generateURLName() {                        // Handles generated URLs.
+async function generateURLName() {                        // Handles generated URL names.
   var i = 0
+  var size = config.minimumURLRandomNameSize
   while (i < 200) {
     var name = ""
 
     var j = 0
-    while (j < config.minimumURLRandomNameSize) {
+    while (j < size) {
       name += config.allowedRandomCharacters[Math.floor(Math.random() * config.allowedRandomCharacters.length)]
       j++
     }
 
-    return name
+    if ((i % 20) == 19) {
+      size += 1
+    }
+
+    if (!fs.existsSync("./database/" + name)) { 
+      return name 
+    }
     i++
   }
 }
 
-app.use(express.static("./public"))
+app.use(express.static("./public"))                     // push static site assets
 
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(express.json());                              // make JSON request bodies work
+
+app.set('trust proxy', true)                            // allows ip access through proxies
 
 app.get('/config.json', function (req, res) {
   res.json({
     minimumURLNameSize: config.minimumURLNameSize,
     maximumURLNameSize: config.maximumURLNameSize,
     allowedRandomCharacters: config.allowedRandomCharacters,
-    passwordProtected: !(!config.password),                   // In a weird way, this kinda converts
-                                                      // the password into a boolean. It works.
+    passwordProtected: !(!config.password),           // In a weird way, this kinda converts
+                                                      // the password into a boolean. It works for now :P
     allowNamedURLs: config.allowNamedURLs,
   })
 })
 
 
-app.post('/create-url', function (req, res) {
+app.post('/create-url', async function (req, res) {
   console.log("NEW REQUEST")
+
+  // validation #1: check if url is valid
 
   var test = /^(?:http[s]?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)$/gm.exec(req.body.url)
   if (!test) {
-    res.json({
+    res.status(400).json({
       success: false,
       error: "url-invalid"
     })
-    res.sendStatus(400)
+    
     return;
   }
 
-  console.log("TEST 1 PASS")
+  // validation #2: if name exists, check if name is valid
 
   var test = RegExp(config.URLNamePattern, "gm").exec(req.body.name)
   if (!test) {
-    res.json({
+    res.status(500).json({
       success: false,
       error: "url-invalid"
     })
-
-    res.sendStatus(400)
 
     return
   }
 
-  console.log("TEST 2 PASS")
+  // validation 3: if a password exists, check if it is correct
 
-  console.log(req.body)
-  console.log(JSON.stringify(req.body))
+  if (config?.password && (req.body.password != config.password)) {
+    res.status(401).json({
+      success: false,
+      error: "password-incorrect"
+    })
 
-  var name = req.body.name || generateURLName()
+    return
+  }
 
-  fs.writeFile('./database/' + name, req.body.url, err => {
+  var name = req.body.name || await generateURLName()
+
+  fs.writeFile('./database/' + name + ".json", JSON.stringify({
+    value: req.body.url,
+    ip: config.logUserData ? req.ip : "logging disabled",
+    userAgent: config.logUserData ? req.headers["user-agent"] : "logging disabled",
+    key: "THIS IS NOT A VALID KEY!!!!!"                         // This is currently unused but it will be eventually
+  }), err => {
     if (err) {
       console.error(err)
-      res.json({
+      res.status(500).json({
         success: false,
         error: err
       })
@@ -94,6 +122,7 @@ app.post('/create-url', function (req, res) {
         success: true,
         name: name
       })
+      console.log("Created new short URL with name "+name+", linking to "+req.body.url+"")
     }
   });
 
@@ -101,14 +130,24 @@ app.post('/create-url', function (req, res) {
 })
 
 app.get('/*', (req, res) => {
-  fs.readFile('./database' + req.path, 'utf8', (err, data) => {
+  fs.readFile('./database' + req.path + ".json", 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      res.send("404 Not Found")
+
+      res.status(404).send("404 Not Found")
+      
       return
     }
+    try {
+      var parsed = JSON.parse(data)
+      res.redirect(parsed.value)
+    } catch(e) {
+      console.error(err)
 
-    res.redirect(data)
+      res.status(500).send("500 Internal Server Error")
+    }
+
+    
   })
 })
 
